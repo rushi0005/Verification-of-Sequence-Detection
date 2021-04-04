3225c3162ed481f586ae438b09d8267a24703fce
rushi0005 <64946315+rushi0005@users.noreply.github.com>
2021-04-03 17:48:31 -0700

    Update FSM.sv

--- FSM.sv
+++ FSM.sv
@@ -1,8 +1,8 @@
-// Code your testbench here
-// or browse Examples
-module seq_det(x,o,clk,rst);
-  input x,clk,reset ;
-  output o ;
+module seq_det(clk,rst,in,out);
+  input in,clk,rst ;
+  output out ;
+  
+  
   
   reg [2:0] state, nextstate ;
   parameter s0 = 3'd0 ;
@@ -11,6 +11,7 @@ module seq_det(x,o,clk,rst);
   parameter s3 = 3'd3 ;
   parameter s4 = 3'd4 ;
   
+  assign out = state == s4 ? 1 : 0 ;
   always @(posedge clk or posedge rst) begin
     if (rst) begin
       state <= s0 ;
@@ -22,51 +23,40 @@ module seq_det(x,o,clk,rst);
   
   always @(*) begin
     case(state)
-      s0 : if (x == 1) begin
+      s0 : if (in == 1) begin
         	nextstate = s1 ;
-            o = 1'b0 ;
+            //out = 1'b0 ;
       end
       else begin
         nextstate = s0 ;
-        o = 1'b0 ;
+        //out = 1'b0 ;
       end
-      s1 : if (x==0) begin
+      s1 : if (in==0) begin
         nextstate = s2 ;
-        o = 1'b0 ;
+        //out = 1'b0 ;
       end
       else begin
         nextstate = s1 ;
-        o = 1'b0 ;
+        //out = 1'b0 ;
       end
-      
-      s2 : if (x==1) begin
+      s2 : if (in==1) begin
         nextstate = s3 ;
-        o = 1'b0 ;
+        //out = 1'b0 ;
       end
       else begin
         nextstate = s0 ;
-        o = 1'b0 ;
+        //out = 1'b0 ;
       end
-      
-      s3 : if (x == 1 ) begin
+      s3 : if (in == 1 ) begin
         
         nextstate = s4 ;
-        o = 1'b0 ;
+        //out = 1'b1 ;
       end
-      
       else begin
         nextstate = s2 ;
-        o = 1'b0 ;
+        //out = 1'b0 ;
       end
-      
-      s4 : if (x==1) begin
-        nextstate = s1 ;
-        o = 1'b0 ;
-      end
-      else begin
-      nextstate = s2   
-      o = 1'b1 ;
-      end
+      s4 : nextstate = s1 ;
     endcase
   end
 endmodule
